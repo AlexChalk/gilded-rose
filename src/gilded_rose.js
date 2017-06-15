7b04ff474b8e586046a65a55a5566198d370f34c
Alex Chalk <alexchalk17@gmail.com>
2017-06-15 12:48:47 -0400

    Refactor updateQuality() to use newly-defined rules
    -Rules include new 'conjured' criterion
    -Rename globalRules array to universalRules
    -Add adjust sellIn method to be run by updateQuality
    -Add 'quality' to name of universalRules
    -Replace mistaken references to 'value' with 'quality' in rules
    -Rewrite default.condition to use 'every' method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -5,25 +5,22 @@ class Item {
     this.quality = quality;
   }
 }
-var newRule = function(params) {
-  var rule = {
-    condition: params.condition,
-    preSellByAction: params.preSellByAction,
-    postSellByAction: params.postSellByAction
-  };
-  rules.push(rule);
-  return rule;
+
+var adjustSellIn = function(item) {
+  if (item.name.includes('Sulfuras') === false) {
+    item.sellIn -= 1;
+  }
 };
 
-var globalRules = {
-  fiftyMax: function(item) {
-    if (item.value > 50) {
-      item.value = 50;
+var universalRules = {
+  fiftyMaxQuality: function(item) {
+    if (item.quality > 50) {
+      item.quality = 50;
     }
   },
-  zeroMin: function(item) {
-    if (item.value < 0) {
-      item.value = 0;
+  zeroMinQuality: function(item) {
+    if (item.quality < 0) {
+      item.quality = 0;
     }
   }
 };
@@ -34,10 +31,10 @@ var rules = {
       return item.name.includes('Conjured');
     },
     preSellByAction: function(item) {
-      item.value -= 2;
+      item.quality -= 2;
     },
     postSellByAction: function(item) {
-      item.value -= 4;
+      item.quality -= 4;
     },
   },
 
@@ -46,10 +43,10 @@ var rules = {
       return item.name === 'Aged Brie';
     },
     preSellByAction: function(item) {
-      item.value += 1;
+      item.quality += 1;
     },
     postSellByAction: function(item) {
-      item.value += 2;
+      item.quality += 2;
     },
   },
 
@@ -71,33 +68,29 @@ var rules = {
     },
     preSellByAction: function(item) {
       if (item.sellIn >= 10) {
-        item.value += 1;
+        item.quality += 1;
       } else if (item.sellIn >= 5) {
-        item.value += 2;
+        item.quality += 2;
       } else if (item.sellIn >= 0) {
-        item.value += 3;
+        item.quality += 3;
       }
     },
     postSellByAction: function(item) {
-      item.value = 0;
+      item.quality = 0;
     },
   },
 
   default: {
     condition: function(item) {
-      var keys = Object.keys(rules).slice(0,-1);
-      for (var i = 0; i < keys.length; i ++ ) {
-        if (rules.keys[i].condition(item)) {
-          return false;
-        }
-      }
-      return true;
+      return Object.keys(rules).slice(0,-1).every(function(key) {
+        return rules[key]['condition'](item) === false;
+      });
     },
     preSellByAction: function(item) {
-      item.value -= 1;
+      item.quality -= 1;
     },
     postSellByAction: function(item) {
-      item.value -= 2;
+      item.quality -= 2;
     },
   }
 };
@@ -107,52 +100,21 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
+    this.items.forEach(function(item) {
+      adjustSellIn(item);
+      Object.keys(rules).forEach(function(rule) {
+        if (rules[rule]['condition'](item)) {
+          if (item.sellIn < 0) {
+            rules[rule]['postSellByAction'](item);
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            rules[rule]['preSellByAction'](item);
           }
         }
-      }
-    }
-
+      });
+      Object.keys(universalRules).forEach(function(uR) {
+        universalRules[uR](item);
+      });
+    });
     return this.items;
   }
 }
