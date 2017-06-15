aabbb6fe51c1c3941da4231354390b0ed027d4ac
Alex Chalk <alexchalk17@gmail.com>
2017-06-15 07:40:04 -0400

    Add specs to verify default behaviour

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,20 @@
 describe('Gilded Rose', function() {
 
-  it('should foo', function() {
-    const gildedRose = new Shop([ new Item('foo', 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual('foo');
+  describe('default item', function() {
+    it('sellIn drops by 1 at each update', function() {
+      const gildedRose = new Shop([ new Item('chocolate', 1, 10) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+    });
+    it('degrades by 1 while sellIn begins day as > 0', function() {
+      const gildedRose = new Shop([ new Item('chocolate', 1, 10) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(9);
+    });
+    it('degrades by 2 when sellIn begins day as <= 0', function() {
+      const gildedRose = new Shop([ new Item('chocolate', 0, 10) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toEqual(8);
+    });
   });
-
 });
