0cc1b184ec210fcf12a8b71e58aa598c3babcff7
Alex Chalk <alexchalk17@gmail.com>
2017-06-15 07:49:26 -0400

    Refactor tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,33 +2,37 @@ describe('Gilded Rose', function() {
 
   describe('default item', function() {
     it('sellIn drops by 1 at each update', function() {
-      const gildedRose = new Shop([ new Item('chocolate', 1, 10) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(0);
+      const item = new Item('chocolate', 1, 10);
+      const gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(item.sellIn).toEqual(0);
     });
     it('degrades by 1 while sellIn begins day as > 0', function() {
-      const gildedRose = new Shop([ new Item('chocolate', 1, 10) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(9);
+      const item = new Item('chocolate', 1, 10);
+      const gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(item.quality).toEqual(9);
     });
     it('degrades by 2 when sellIn begins day as <= 0', function() {
-      const gildedRose = new Shop([ new Item('chocolate', 0, 10) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(8);
+      const item = new Item('chocolate', 0, 10);
+      const gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(item.quality).toEqual(8);
     });
   });
 
   describe('aged brie', function() {
     it('improves by 1 while sellIn begins day as > 0', function() {
-      const gildedRose = new Shop([ new Item('Aged Brie', 1, 10) ]);
-      const items = gildedRose.updateQuality();
-      console.log(items[0].sellIn);
-      expect(items[0].quality).toEqual(11);
+      const item = new Item('Aged Brie', 1, 10);
+      const gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(item.quality).toEqual(11);
     });
     it('improves by 2 when sellIn begins day as <= 0', function() {
-      const gildedRose = new Shop([ new Item('Aged Brie', 0, 10) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].quality).toEqual(12);
+      const item = new Item('Aged Brie', 1, 10);
+      const gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(item.quality).toEqual(12);
     });
   });
 
